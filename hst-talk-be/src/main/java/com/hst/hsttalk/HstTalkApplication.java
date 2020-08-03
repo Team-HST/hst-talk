4f79507b15c618cd1aa81d52414f358d13c2c45a
HyunGyu-Lee <gusrb0808@naver.com>
2020-08-04 00:27:26 +0900

    [Fix #1] 프로젝트 최초 업로드

---
+++ hst-talk-be/src/main/java/com/hst/hsttalk/HstTalkApplication.java
@@ -0,0 +1,13 @@
+package com.hst.hsttalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HstTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HstTalkApplication.class, args);
+	}
+
+}
---
+++ hst-talk-be/src/test/java/com/hst/hsttalk/HstTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hst.hsttalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HstTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
