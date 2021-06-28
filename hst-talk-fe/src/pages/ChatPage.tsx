89c746b96aadafbb2b952fd18459dfa75971cc3c
dudgns0612 <dudgns0612@naver.com>
2021-06-28 22:00:33 +0900

    [F/E] 채팅 페이지 사이드 작업

--- hst-talk-fe/src/pages/ChatPage.tsx
+++ hst-talk-fe/src/pages/ChatPage.tsx
@@ -1,5 +1,28 @@
+import styles from 'resources/css/chat.module.css';
+
 const ChatPage = () => {
-  return <div>chat page</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={`${styles.chat_info} ${styles.on}`}>
+        <div className={styles.chat_info_header}>
+          <h2>HST </h2>
+          <span>Talk</span>
+        </div>
+        <div className={styles.chat_info_body}>
+          <div>🟢 김영후이</div>
+          <div>🟢 이현규이</div>
+        </div>
+        <div className={styles.chat_info_footer}>
+          <h3>
+            Room Code{' '}
+            <i className={`far fa-copy ${styles.chat_info_footer_copy}`} title="code copy"></i>
+          </h3>
+          <div>hst_yh_sg123dvxw</div>
+        </div>
+      </div>
+      <div className={styles.chat_box}></div>
+    </div>
+  );
 };
 
 export default ChatPage;
