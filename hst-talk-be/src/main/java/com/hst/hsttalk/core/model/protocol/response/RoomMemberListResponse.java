7ea32dddbd5b2383ce3000c845e880ec29010d91
HyunGyu-Lee <gusrb0808@naver.com>
2021-07-09 17:07:29 +0900

    방 참가자 목록 조회하는 프로토콜 추가

--- hst-talk-be/src/main/java/com/hst/hsttalk/core/model/protocol/response/RoomMemberListResponse.java
+++ hst-talk-be/src/main/java/com/hst/hsttalk/core/model/protocol/response/RoomMemberListResponse.java
@@ -1,7 +1,31 @@
 package com.hst.hsttalk.core.model.protocol.response;
 
+import com.hst.hsttalk.core.model.user.ChatUser;
+import com.hst.hsttalk.core.model.user.ChatUser.ChatUserResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
 /**
  * @author dlgusrb0808@gmail.com
  */
+@Getter
+@AllArgsConstructor(staticName = "of")
 public class RoomMemberListResponse {
+	private ChatUserResponse owner;
+	private List<RoomMemberResponse> participants;
+
+	@Getter
+	public static class RoomMemberResponse {
+		private boolean me;
+		private ChatUserResponse userInfo;
+
+		public static RoomMemberResponse of(String id, ChatUser user) {
+			RoomMemberResponse response = new RoomMemberResponse();
+			response.userInfo = user.toResponse();
+			response.me = response.userInfo.getId().equals(id);
+			return response;
+		}
+	}
 }
