f3fd0090266f821c4d73ca09e4904945dbba4d6a
HyunGyu-Lee <gusrb0808@naver.com>
2021-07-14 20:56:09 +0900

    방 나가기 프로토콜 추가

--- hst-talk-be/src/main/java/com/hst/hsttalk/core/model/action/impl/LeaveRoomAction.java
+++ hst-talk-be/src/main/java/com/hst/hsttalk/core/model/action/impl/LeaveRoomAction.java
@@ -4,6 +4,7 @@ import com.hst.hsttalk.core.SessionContextHolder;
 import com.hst.hsttalk.core.model.action.Action;
 import com.hst.hsttalk.core.model.messaging.MessageProtocol;
 import com.hst.hsttalk.core.model.protocol.response.ChatResponse;
+import com.hst.hsttalk.core.model.protocol.response.RoomMemberListResponse;
 import com.hst.hsttalk.core.model.room.ChatRoom;
 import com.hst.hsttalk.core.model.roommanager.RoomManager;
 import com.hst.hsttalk.core.model.roommanager.RoomManagerAware;
@@ -13,6 +14,8 @@ import com.hst.hsttalk.core.model.user.ConnectedUserPool;
 import com.hst.hsttalk.core.model.user.ConnectedUserPoolAware;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
+
+import java.util.stream.Collectors;
 
 /**
  * Copyright 2021 NHN Corp. All rights Reserved.
@@ -31,25 +34,23 @@ public class LeaveRoomAction implements Action, RoomManagerAware, ConnectedUserP
 		String roomId = protocol.getRoomId();
 		ChatUser user = pool.get(SessionContextHolder.getCurrentSession().getId());
 		ChatRoom room = roomManager.getRoom(protocol.getRoomId());
+		String ownerId = room.getRoomOwner().getSession().getId();
 
 		room.leave(user);
 		pool.remove(session);
-		//
-		//		RoomMemberListResponse roomMemberListResponse = RoomMemberListResponse.of(room.getRoomOwner()
-		//		.toResponse(),
-		//				room.getParticipants().stream().map(e -> RoomMemberListResponse.RoomMemberResponse.of(roomId,
-		//				e)).collect(Collectors.toList()));
+
+		RoomMemberListResponse roomMemberListResponse =
+				RoomMemberListResponse.of(room.getParticipants().stream().map(e -> e.toResponse(ownerId)).collect(Collectors.toList()));
 
 		// 해당 방에 노티
 		for (ChatUser participant : room.getParticipants()) {
-			String chatMessage = String.format("%s님이 퇴장하셨습니다.", user.getNickname());
+			String chatMessage = String.format("%s님이 퇴장하셨습니다ㅠㅠ", user.getNickname());
 			ChatResponse response = ChatResponse.of("SYSTEM", chatMessage, false);
 			TextMessage responseProtocol =
 					MessageProtocol.of(MessageType.CHAT, protocol.getRoomId(), response).toTextMessage();
 			participant.getSession().sendMessage(responseProtocol);
-			//			participant.getSession().sendMessage(MessageProtocol.of(MessageType.GET_ROOM_MEMBER_LIST,
-			//			roomId,
-			//					roomMemberListResponse).toTextMessage());
+			participant.getSession().sendMessage(MessageProtocol.of(MessageType.GET_ROOM_MEMBER_LIST, roomId,
+					roomMemberListResponse).toTextMessage());
 		}
 	}
 
